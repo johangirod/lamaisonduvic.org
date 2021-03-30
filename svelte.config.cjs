5f7ad55860a6f87eec0866ecf4bc8e642b8b1af2
Johan Girod <dev@johangirod.com>
2021-03-30 15:29:54 +0200

    Use vercel for deployment

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,26 +1,23 @@
-const { markdown } = require('svelte-preprocess-markdown');
-const node = require('@sveltejs/adapter-node');
-const pkg = require('./package.json');
+const { markdown } = require("svelte-preprocess-markdown");
+const vercel = require("@sveltejs/adapter-vercel");
+const pkg = require("./package.json");
 
 /** @type {import('@sveltejs/kit').Config} */
 module.exports = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: markdown(),
-	extensions: ['.svelte', '.md'],
-	kit: {
-		// By default, `npm run build` will create a standard Node app.
-		// You can create optimized builds for different platforms by
-		// specifying a different adapter
-		adapter: node(),
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: markdown(),
+  extensions: [".svelte", ".md"],
+  kit: {
+    adapter: vercel(),
 
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: "#svelte",
 
-		vite: {
-			ssr: {
-				noExternal: Object.keys(pkg.dependencies || {})
-			}
-		}
-	}
+    vite: {
+      ssr: {
+        noExternal: Object.keys(pkg.dependencies || {}),
+      },
+    },
+  },
 };
